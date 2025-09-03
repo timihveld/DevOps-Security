15280e49978f1a57949d596e11adced2f45d4835
timihveld <516886@student.saxion.nl>
2025-09-03 13:10:07 +0200

    Update quoter_templates.py

--- content/quoter_templates.py
+++ content/quoter_templates.py
@@ -60,7 +60,7 @@ def page(content,user_id,title,error=None):
 """
     else:
         links = f"""
-<label class="link" for="signinCheckbox">hier kun je uitschrijven</label>
+<label class="link" for="signinCheckbox">Inloggen</label>
 """
 
     return f"""<!DOCTYPE html>
