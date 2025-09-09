efc8bfe188258ef6cced12ae5c892bc3bf9143bd
timihveld <516886@student.saxion.nl>
2025-09-09 13:22:59 +0200

    Update quoter_templates.py

--- content/quoter_templates.py
+++ content/quoter_templates.py
@@ -60,7 +60,7 @@ def page(content,user_id,title,error=None):
 """
     else:
         links = f"""
-<label class="link" for="signinCheckbox">Uitloggen</label>
+<label class="link" for="signinCheckbox">Tim in het Veld - 516886</label>
 """
 
     return f"""<!DOCTYPE html>
