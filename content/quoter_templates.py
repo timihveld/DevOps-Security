e96ca6a0466027371f19547a25d7d79a63ee5c60
timihveld <516886@student.saxion.nl>
2025-09-09 16:14:40 +0200

    Update quoter_templates.py

--- content/quoter_templates.py
+++ content/quoter_templates.py
@@ -60,7 +60,7 @@ def page(content,user_id,title,error=None):
 """
     else:
         links = f"""
-<label class="link" for="signinCheckbox">test met security groups</label>
+<label class="link" for="signinCheckbox">test met security groups 2</label>
 """
 
     return f"""<!DOCTYPE html>
