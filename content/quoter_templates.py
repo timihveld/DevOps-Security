1b95777ea9c5e870191ac8b4834c94cfed6cb20e
timihveld <516886@student.saxion.nl>
2025-09-09 13:31:58 +0200

    Update quoter_templates.py

--- content/quoter_templates.py
+++ content/quoter_templates.py
@@ -60,7 +60,7 @@ def page(content,user_id,title,error=None):
 """
     else:
         links = f"""
-<label class="link" for="signinCheckbox">Tim in het Veld - 516886</label>
+<label class="link" for="signinCheckbox">test met dockerhub private</label>
 """
 
     return f"""<!DOCTYPE html>
